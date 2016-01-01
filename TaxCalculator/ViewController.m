55f7b414a6fa4f9396417d4d084a9b9c2b685271
Rafael Auriemo <rauriemo@Rafaels-MacBook-Pro.local>
2016-01-01 01:49:16 -0500

    tax calculator first commit

--- TaxCalculator/ViewController.m
+++ TaxCalculator/ViewController.m
@@ -10,18 +10,39 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *priceTextField;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+
+@property double nyTax, chiTax, caTax;
+- (IBAction)onCalculateButtonTapped:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIButton *button;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.caTax = .075f;
+    self.chiTax = .0925f;
+    self.nyTax = .045f;
+    self.resultLabel.text = @"";
+    self.button.layer.cornerRadius = 10;
+    self.button.clipsToBounds = YES;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)onCalculateButtonTapped:(id)sender {
+    
+    if(self.segmentedControl.selectedSegmentIndex == 0){
+        self.resultLabel.text = [NSString stringWithFormat:@"$%.02f", self.caTax*[self.priceTextField.text doubleValue]];
+    }else if (self.segmentedControl.selectedSegmentIndex == 1){
+        self.resultLabel.text = [NSString stringWithFormat:@"$%.02f", self.chiTax*[self.priceTextField.text doubleValue]];
+    }else{
+        self.resultLabel.text = [NSString stringWithFormat:@"$%.02f", self.nyTax*[self.priceTextField.text integerValue]];
+    }
 }
-
 @end
